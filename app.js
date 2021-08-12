0c9ae456244d10d5402376cf20fa3b8aa417aeb0
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-08-12 03:32:04 -0300

    Foi adicionado o nodemon

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const app = express();
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.send("Portal de notícias");
+    res.render("home/index");
 });
 
-app.get("/tecnologia", (req, res) => {
-    res.render("secao/tecnologia")
+app.get("/formulario_inclusao_noticia", (req, res) => {
+    res.render("admin/form_add_noticia")
 });
 
-app.get("/moda", (req, res) => {
-    res.send("Noticícias de moda")
+app.get("/noticias", (req, res) => {
+    res.render("noticias/noticias");
 });
 
-app.get("/beleza", (req, res) => {
-    res.send("Noticícias de beleza")
+app.get("/noticia", (req, res) => {
+    res.render("noticias/noticia");
 });
 
 app.listen("3000", () => console.log("Servidor rodando com Express"));
