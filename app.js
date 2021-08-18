1d47dd13ce3a386133405d7d0e2e10dd98a733d3
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-08-18 02:28:16 -0300

    Foi adicionado o mysql

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const app = require("./config/server");
 
-const rotaHome = require("./app/routes/home")
-rotaHome(app);
-
-const rotaNoticias = require("./app/routes/noticias")
-rotaNoticias(app);
-
-const rotaFormularioInclusao = require("./app/routes/formulario_inclusao_noticia")
-rotaFormularioInclusao(app);
+const rotaHome = require("./app/routes/home")(app);
+const rotaNoticias = require("./app/routes/noticias")(app);
+const rotaFormularioInclusao = require("./app/routes/formulario_inclusao_noticia")(app);
 
 
 app.listen("3000", () => console.log("Servidor está rodando"));
